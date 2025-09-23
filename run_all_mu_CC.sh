a40dcb94f48c64c8375c83c9a8a2a0215907e7cc
Robert-Mihai Amarinei <amarinei@login3.int.fir.alliancecan.ca>
2025-09-23 02:49:26 -0700

    version used to generated 250k events

--- run_all_mu_CC.sh
+++ run_all_mu_CC.sh
@@ -4,14 +4,15 @@ cd /scratch/amarinei/LArSoft_scripts
 NUM_EVENTS=250
 NUM_FILES=1000
 MEMORY=35
-DATA_DIR="/scratch/amarinei/data/Atmospherics/MuCC_$NUM_EVENTS"
+DATA_DIR="/scratch/amarinei/data/Atmospherics/MuCC_${NUM_EVENTS}_${NUM_FILES}"
 LOG_DIR="$DATA_DIR/logs"
 FCL_FILE_NAME="prodgenie_atmnMuCC_max_weighted_dune10kt_1x2x6"
 
 mkdir -p "$DATA_DIR" "$LOG_DIR"
 
-COMMON_SBATCH_OPTS="--account=def-nilic --time=0:30:00 --mem=${MEMORY}G --mail-user=robert.mihai.amarinei@cern.ch --mail-type=BEGIN,END,FAIL --array=1-$NUM_FILES"
-DETSIM_SBATCH_OPTS="--account=def-nilic --time=0:30:00 --mem=${MEMORY}G --mail-user=robert.mihai.amarinei@cern.ch --mail-type=BEGIN,END,FAIL --array=1-$NUM_FILES"
+LOG_SBATCH_OPTS="--account=def-nilic --time=0:05:00 --mem=1G --mail-user=robert.mihai.amarinei@cern.ch --mail-type=BEGIN,END,FAIL"
+COMMON_SBATCH_OPTS="--account=def-nilic --time=12:00:00 --mem=${MEMORY}G --mail-user=robert.mihai.amarinei@cern.ch --mail-type=BEGIN,END,FAIL --array=1-$NUM_FILES"
+DETSIM_SBATCH_OPTS="--account=def-nilic --time=12:00:00 --mem=${MEMORY}G --mail-user=robert.mihai.amarinei@cern.ch --mail-type=BEGIN,END,FAIL --array=1-$NUM_FILES"
 
 job1=$(sbatch $COMMON_SBATCH_OPTS -J mu_gen_events --output=$LOG_DIR/slurm-%x-%j.out --error=$LOG_DIR/slurm-%x-%j.err run_gen_events.sh $NUM_EVENTS $FCL_FILE_NAME "$DATA_DIR" | awk '{print $4}')
 job2=$(sbatch $COMMON_SBATCH_OPTS -J mu_g4 --dependency=afterok:$job1 --output=$LOG_DIR/slurm-%x-%j.out --error=$LOG_DIR/slurm-%x-%j.err run_g4.sh $NUM_EVENTS "$DATA_DIR" $FCL_FILE_NAME | awk '{print $4}')
@@ -24,7 +25,7 @@ job5=$(sbatch $COMMON_SBATCH_OPTS -J mu_makehdf5 --dependency=afterok:$job4 --ou
 
 # Retrieve and log timing info for all jobs after completion
 # sacct -j $job1,$job2,$job3,$job4,$job5 --format=JobID,JobName,Start,End,Elapsed,MaxRSS > $LOG_DIR/job_times.log
-job6=$(sbatch --dependency=afterok:$job5 --output=$LOG_DIR/jobtime_%j.out --error=$LOG_DIR/jobtime_%j.err --wrap="sacct -j $job1,$job2,$job3,$job4,$job5 --format=JobID,JobName,Start,End,Elapsed,MaxRSS > $LOG_DIR/job_times.log && cat $LOG_DIR/job_times.log" | awk '{print $4}')
+job6=$(sbatch $LOG_SBATCH_OPTS --dependency=afterok:$job5 --output=$LOG_DIR/jobtime_%j.out --error=$LOG_DIR/jobtime_%j.err --wrap="sacct -j $job1,$job2,$job3,$job4,$job5 --format=JobID,JobName,Start,End,Elapsed,MaxRSS > $LOG_DIR/job_times.log && cat $LOG_DIR/job_times.log" | awk '{print $4}')
 
 # Print the timing log to terminal
 cat $LOG_DIR/job_times.log
