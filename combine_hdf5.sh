f1d2dee5bf5a7a971d4b208159a9145406d5c5a0
ramarine <robert.amarinei@icloud.com>
2026-01-06 01:08:11 -0800

    Added the capacity to ahve more than one intput folder (-i dir1 -i dir2 etc...). This will basically combine the h5 file from all the interactions generated and further the code will work the same as previously

--- combine_hdf5.sh
+++ combine_hdf5.sh
@@ -1,39 +1,75 @@
 #! /bin/bash
 
-###THIS SCRIPTS WILL CONCATENATE HDF5 FILES IN A GIVEN DIRECTORY
-
-# path to files to concat = /scratch/amarinei/data/Atmospherics/TauCC_new/hdf5
-# path to output 
-# INFILE_DIR=$1 
-# OUT_NAME=$2
+###THIS SCRIPT WILL CONCATENATE HDF5 FILES FROM MULTIPLE DIRECTORIES
 
 module load StdEnv/2020
 module load apptainer/1.1.8
-# source /project/6079563/neutrino_ml/LArSoft_scripts/setup_LArSoft_area_cc.sh /project/6071458/neutrino_ml/LArSoft_v09_83_01d00
 source /scratch/amarinei/LArSoft_scripts/setup_LArSoft_area_cc.sh /home/amarinei/Software/LArSoft_v10_09_00d00
 
-# INFILE_DIR="/scratch/amarinei/data/Atmospherics/TauCC_10/hdf5"
-INFILE_DIR=$1
-OUT_NAME=$2
-OUTFILE_DIR=$3
+# Parse arguments: all directories are passed first, then OUT_NAME and OUTFILE_DIR
+NUM_ARGS=$#
 
+# Extract OUT_NAME and OUTFILE_DIR (last two arguments)
+OUT_NAME="${@: -2:1}"
+OUTFILE_DIR="${@: -1}"
+
+# Build array of input directories (all but last two arguments)
+INFILE_DIRS=("${@:1:$((NUM_ARGS-2))}")
 
 PATH_TO_NUML="/project/6079563/neutrino_ml/Numl_Image/numl:v23.11.0.sif"
 USERNAME="amarinei"
 
+# Create temporary file list in output directory
+CONCAT_LIST="${OUTFILE_DIR}/files_to_concat.txt"
+> "$CONCAT_LIST"  # Clear file if it exists
 
-for f in ${INFILE_DIR}/*.h5
-do
-	echo $f >> ${INFILE_DIR}/files_to_concat.txt
+echo "Collecting HDF5 files from ${#INFILE_DIRS[@]} directories:"
+
+# Loop through all input directories and collect .h5 files
+for INFILE_DIR in "${INFILE_DIRS[@]}"; do
+  echo "  - Scanning: $INFILE_DIR"
+  
+  # Check if directory exists
+  if [ ! -d "$INFILE_DIR" ]; then
+    echo "Warning: Directory does not exist: $INFILE_DIR"
+    continue
+  fi
+  
+  # Count and add files from this directory
+  file_count=0
+  for f in "${INFILE_DIR}"/*.h5; do
+    # Check if glob matched any files
+    [ -e "$f" ] || continue
+    echo "$f" >> "$CONCAT_LIST"
+    ((file_count++))
+  done
+  
+  echo "    Found $file_count files"
 done
 
+# Check if we found any files
+total_files=$(wc -l < "$CONCAT_LIST")
+if [ "$total_files" -eq 0 ]; then
+  echo "Error: No HDF5 files found in any of the input directories"
+  rm "$CONCAT_LIST"
+  exit 1
+fi
+
+echo ""
+echo "Total files to concatenate: $total_files"
+echo "Output file: ${OUTFILE_DIR}/${OUT_NAME}.gnn.h5"
+echo ""
 echo "Running ph5_concat ..."
 
-singularity exec --cleanenv --bind ${INFILE_DIR}:/numl_data --bind /project/6079563 --bind /scratch/${USERNAME} --nv ${PATH_TO_NUML} mpiexec -n 32 ph5_concat -i ${INFILE_DIR}/files_to_concat.txt -o ${OUTFILE_DIR}/${OUT_NAME}.gnn.h5
+# Build bind mount arguments for all unique parent directories
+BIND_ARGS=""
+for INFILE_DIR in "${INFILE_DIRS[@]}"; do
+  BIND_ARGS="${BIND_ARGS} --bind ${INFILE_DIR}:/numl_data_$(basename ${INFILE_DIR})"
+done
+
+singularity exec --cleanenv ${BIND_ARGS} --bind ${OUTFILE_DIR}:/numl_output --bind /project/6079563 --bind /scratch/${USERNAME} --nv ${PATH_TO_NUML} mpiexec -n 32 ph5_concat -i ${CONCAT_LIST} -o ${OUTFILE_DIR}/${OUT_NAME}.gnn.h5
 
 echo "ph5_concat done"
 
-rm  ${INFILE_DIR}/files_to_concat.txt
-echo "Done" 
-
-
+rm "$CONCAT_LIST"
+echo "Done"
--- run_conc_add_proc.sh
+++ run_conc_add_proc.sh
@@ -2,8 +2,14 @@
 
 usage() {
   echo ""
-  echo "Usage: $0 -i INFILE_DIR -o OUT_NAME"
-  echo "Example: sbatch --account=def-nilic $0 -i /scratch/amarinei/data/Atmospherics/TauCC_1_50/hdf5_reco1 -n concatenated -o /scratch/amarinei/data/Atmospherics/TauCC_1_50/hdf5_reco1"
+  echo "Usage: $0 -i INFILE_DIR [-i INFILE_DIR ...] -o OUT_NAME -n OUT_NAME"
+  echo "Example: sbatch --account=def-nilic $0 -i /scratch/amarinei/data/Atmospherics/TauCC_1_50/hdf5_reco1 -i /scratch/amarinei/data/Atmospherics/TauCC_1_50/hdf5_reco2 -n concatenated -o /scratch/amarinei/data/Atmospherics/TauCC_1_50/output"
+  echo ""
+  echo "Options:"
+  echo "  -i INFILE_DIR    Input directory (can be specified multiple times)"
+  echo "  -n OUT_NAME      Output name"
+  echo "  -o OUTFILE_DIR   Output directory"
+  echo "  -h               Show this help message"
   echo ""
   exit 1
 }
@@ -13,10 +19,13 @@ if [ $# -eq 0 ]; then
   usage
 fi
 
+# Initialize array for input directories
+INFILE_DIRS=()
+
 # Parse command line options
 while getopts ":i:o:n:h" opt; do
   case $opt in
-    i) INFILE_DIR="$OPTARG" ;;
+    i) INFILE_DIRS+=("$OPTARG") ;;
     n) OUT_NAME="$OPTARG" ;;
     o) OUTFILE_DIR="$OPTARG" ;;
     h) usage ;;
@@ -25,29 +34,32 @@ while getopts ":i:o:n:h" opt; do
   esac
 done
 
-if [ -z "${INFILE_DIR:-}" ] || [ -z "${OUT_NAME:-}" ] || [ -z "${OUTFILE_DIR:-}" ]; then
+# Validation
+if [ ${#INFILE_DIRS[@]} -eq 0 ] || [ -z "${OUT_NAME:-}" ] || [ -z "${OUTFILE_DIR:-}" ]; then
   echo "Error: Missing required arguments."
   usage
 fi
 
+echo "Processing ${#INFILE_DIRS[@]} input directories:"
+for dir in "${INFILE_DIRS[@]}"; do
+  echo "  - $dir"
+done
+
 # Directories and user info
 USERNAME="amarinei"
-LOG_DIR="${INFILE_DIR}/logs_proc"
+mkdir -p "$OUTFILE_DIR"
+LOG_DIR="${OUTFILE_DIR}/logs_proc"
 mkdir -p "$LOG_DIR"
 
 # Common SLURM options
 COMMON_OPTS="--account=def-nilic --mail-user=robert.mihai.amarinei@cern.ch --mail-type=BEGIN,END,FAIL"
 
-STEP1_OPTS="--time=0:30:00 --mem=12G --nodes=1 --ntasks=32 $COMMON_OPTS"
-STEP2_OPTS="--time=1:30:00 --mem=12G $COMMON_OPTS"
-STEP3_OPTS="--time=1:30:00 --mem=12G --nodes=1 --ntasks=32 $COMMON_OPTS"
+STEP1_OPTS="--time=0:30:00 --mem=32G --nodes=1 --ntasks=32 $COMMON_OPTS"
+STEP2_OPTS="--time=1:30:00 --mem=32G $COMMON_OPTS"
+STEP3_OPTS="--time=1:30:00 --mem=32G --nodes=1 --ntasks=32 $COMMON_OPTS"
 
-# STEP1_OPTS="--time=0:30:00 --mem=32G --nodes=1 --ntasks=32 $COMMON_OPTS"
-# STEP2_OPTS="--time=12:30:00 --mem=32G $COMMON_OPTS"
-# STEP3_OPTS="--time=12:30:00 --mem=32G --nodes=1 --ntasks=32 $COMMON_OPTS"
-
-# Submit job 1
-job1=$(sbatch $STEP1_OPTS --output=${LOG_DIR}/combine_hdf5_%j.out --error=${LOG_DIR}/combine_hdf5_%j.err ./combine_hdf5.sh "$INFILE_DIR" "$OUT_NAME" "$OUTFILE_DIR" | awk '{print $4}')
+# Submit job 1 - pass all directories as arguments
+job1=$(sbatch $STEP1_OPTS --output=${LOG_DIR}/combine_hdf5_%j.out --error=${LOG_DIR}/combine_hdf5_%j.err ./combine_hdf5.sh "${INFILE_DIRS[@]}" "$OUT_NAME" "$OUTFILE_DIR" | awk '{print $4}')
 
 # Submit job 2, dependent on step 1
 job2=$(sbatch $STEP2_OPTS --dependency=afterok:$job1 --output=${LOG_DIR}/add_key_%j.out --error=${LOG_DIR}/add_key_%j.err ./add_key.sh "$OUTFILE_DIR" "$OUT_NAME" | awk '{print $4}')
@@ -57,3 +69,10 @@ job3=$(sbatch $STEP3_OPTS --dependency=afterok:$job2 --output=${LOG_DIR}/process
 
 # Submit job 4, dependent on step 3
 job4=$(sbatch $STEP3_OPTS --dependency=afterok:$job3 --output=${LOG_DIR}/merge_%j.out --error=${LOG_DIR}/merge_%j.err ./merge.sh "$OUTFILE_DIR" "$OUT_NAME" | awk '{print $4}')
+
+# echo ""
+# echo "Jobs submitted:"
+# echo "  Job 1 (combine_hdf5): $job1"
+# echo "  Job 2 (add_key):      $job2"
+# echo "  Job 3 (process):      $job3"
+# echo "  Job 4 (merge):        $job4"
